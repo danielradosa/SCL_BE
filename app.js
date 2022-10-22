d8081b9a22cd6c4413c8c0381614a23b7518db63
Daniel Radosa <danysko5@gmail.com>
2022-10-22 21:09:06 +0200

    Enabling CORS

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ const port = process.env.PORT || 1000;
 const db_url = process.env.MONGO_DB;
 const app = express();
 
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+// Connect to MongoDB
 mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
 });
 
+// Format response as JSON
 app.use(express.json());
 
+// Start up server
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: false
+    graphiql: true
 }));
 
 app.listen(port, () => {
