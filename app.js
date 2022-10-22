80e357841031ebb56c33fe490d51f28318f411d1
Daniel Radosa <danysko5@gmail.com>
2022-10-22 21:25:00 +0200

    LAST TRY TODAY?

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -15,13 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-*");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true });
